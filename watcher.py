0815262950bafccb8db63cb86c254702a27a0d16
Darren Paul <darren.paul@sunriseproductions.tv>
2018-07-15 18:55:11 +0200

    catagorised base currencies

--- watcher.py
+++ watcher.py
@@ -11,18 +11,22 @@ import binance
 
 class Watcher:
     def __init__(self):
-        self.tradingPairs = {}
+        self.wallet = Wallet()
+        self.tradingPairs = self._get_trading_pairs()
         self.currencyObjects = []
         self.topCurrencies = {}
 
     def create_currency_objects(self):
-        _priceData = self._get_price_data()
-        for item in _priceData:
+        for item in self._get_price_data():
             _obj = Currency(pair=item["pair"], start_price=item["last"], start_time=item["last"])
+            self.wallet
+            setattr(self.wallet, item["pair"], _obj)
             self.currencyObjects.append(_obj)
+
 
     def update_top_currencies(self):
         _data = {}
+
         for item in self._get_price_data():
             _pair = item["pair"]
             _price = item["last"]
@@ -31,32 +35,55 @@ class Watcher:
                     _change = obj.check_price(price=_price)
                     _data.update({_pair: _change})
 
-        _data = OrderedDict(sorted(_data.items(), key=itemgetter(1), reverse=True))
+        _data = self._pair_currencies(data=_data)
 
-        self._format_top_items(data=_data)
-        self.display_top_currencies()
+        for key, val in _data.items():
+            _data[key] = OrderedDict(sorted(_data[key].items(), key=itemgetter(1), reverse=True))
 
-    def _format_top_items(self, data):
-        self.topCurrencies = {}
+        for key, val in _data.items():
+            self._format_top_items(base_currency=key, data=val)
+
+    def _pair_currencies(self, data):
+        _data = {}
+        for key, val in self.tradingPairs.items():
+            _base = key
+            for quote in val:
+                _pair = "{quote}{base}".format(quote=quote, base=_base)
+                for pair, per in data.items():
+                    if pair == _pair:
+                        if _base not in _data.keys():
+                            _data.update({_base: {}})
+                        _data[_base].update({pair: per})
+        return _data
+
+    def _format_top_items(self, base_currency, data):
+        _topCurrencies = {}
         _keys = list(data.keys())
         for i in range(0, 10):
             _pair = _keys[i]
             _change = data[_keys[i]]
-            self.topCurrencies.update({_pair: _change})
-        self.topCurrencies = OrderedDict(sorted(self.topCurrencies.items(), key=itemgetter(1), reverse=True))
+            _topCurrencies.update({_pair: _change})
+        _topCurrencies = OrderedDict(sorted(_topCurrencies.items(), key=itemgetter(1), reverse=True))
+        self.display_top_currencies(base_currency=base_currency, data=_topCurrencies)
 
-    def display_top_currencies(self):
+    def display_top_currencies(self, base_currency, data):
+        print("="*100)
+        print("Base currency {base}".format(base=base_currency))
         print("Updated at {_time}".format(_time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
         print("="*100)
-        for key, val in self.topCurrencies.items():
+        for key, val in data.items():
             print("pair: {pair}, price: {price}".format(pair=key, price=val))
+            print("start price: {price}".format(price=getattr(self.wallet, key).startPrice))
+            print("last price: {price}".format(price=getattr(self.wallet, key).lastPrice))
+            print("-"*100)
         print("="*100)
         print("\n")
+
     def _get_price_data(self):
         return binance.get_price()
 
     def _get_trading_pairs(self):
-        self.tradingPairs = binance.get_trading_pairs()
+        return copy.deepcopy(binance.get_trading_pairs())
 
 
 class Currency:
@@ -64,27 +91,30 @@ class Currency:
         self.pair = pair
         self.startPrice = start_price
         self.startTime = start_time
-        self.percentageChange = ""
+        self.lastPrice = ""
 
     def check_price(self, price):
+        self.lastPrice = price
         _startPrice = float(self.startPrice)
-        _currentPrice = float(price)
-        return str(get_percentage_difference(value1=_startPrice, value2=_currentPrice))
+        _currentPrice = float(self.lastPrice)
+        return str(get_percentage_change(new_value=_currentPrice, original_value=_startPrice))
 
+class Wallet:
+    def __init__(self):
+        pass
 
 def get_difference(value1, value2):
     return float(value1) - float(value2)
 
 
-def get_percentage_difference(value1, value2):
-    difference = get_difference(value1=value1, value2=value2)
-    average = float(value1) + float(value2) / 2
+def get_percentage_change(new_value, original_value):
+    difference = get_difference(value1=new_value, value2=original_value)
 
-    return (difference / average) * 100
+    return (difference / original_value) * 100
 
 a = False
 watcherObj = Watcher()
 watcherObj.create_currency_objects()
 while a is False:
     watcherObj.update_top_currencies()
-    time.sleep(3600)
+    time.sleep(1800)
