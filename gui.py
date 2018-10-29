06e54e83fcc076482ec9fcd3809c177728493bce
Darren Paul <darren.paul@triggerfish.co.za>
2018-10-29 16:59:11 +0200

    working on watcher bot

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import os
 import re
+import time
 import sys
 import glob
 from pprint import pprint
@@ -11,6 +12,8 @@ except ImportError:
     from PySide.QtCore import *
     from PySide.QtGui import *
 
+from modules.binance import binance
+
 # ROOTDIR = os.path.dirname(os.path.abspath(__file__))
 # sys.path.append(ROOTDIR.replace("{sep}environment".format(sep=os.sep), ""))
 # from utils import utils, client_manager, temp_manager
@@ -20,8 +23,16 @@ except ImportError:
 class Launcher(QMainWindow):
     def __init__(self, parent=None):
         super(Launcher, self).__init__(parent)
+
         self.__build_ui()
         self.__style_interface()
+        self.__build_widgets()
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.updatePrices)
+        timer.start(10000)
+        self.updatePrices()
+
 
     def __build_ui(self):
         self.__build_windows()
@@ -29,7 +40,7 @@ class Launcher(QMainWindow):
 
     def __build_windows(self):
         self.setWindowTitle("Launcher")
-        self.setFixedSize(300, 600)
+        self.setFixedSize(600, 600)
 
     def __build_widgets(self):
         self.widgets = Widgets()
@@ -39,16 +50,21 @@ class Launcher(QMainWindow):
         QApplication.setStyle(QStyleFactory.create('plastique'))
         self.setStyleSheet("background-color:#263238; color:#ececec;")
 
+    def updatePrices(self):
+        self.widgets.update_watchers()
+
 
 class Widgets(QWidget):
     def __init__(self):
         super(Widgets, self).__init__()
 
-        self.__load_temp()
+        self.watchers = []
+
+        self.prices = binance.get_price()
 
         self.grid = QGridLayout()
 
-        self.__build_client()
+        self.__new_currency()
 
     def __build_client(self):
         if hasattr(self, "clientGroup"):
@@ -85,13 +101,102 @@ class Widgets(QWidget):
             if index >= 0:
                 self.projectCb.setCurrentIndex(index)
 
+    def __new_currency(self):
+        self.newCurrencyGroup = QGroupBox("")
+        self.newCurrencyGroup.setStyleSheet("QGroupBox{border: 0px;};")
+        self.pairs = QComboBox(self)
+        self.pairs.addItems(_sort_pairs(binance.get_trading_pairs()))
+        self.addCurrencyButton = QPushButton("Add")
+        self.addCurrencyButton.clicked.connect(self.__add_new_currency_connect)
 
+        grid = QGridLayout()
+        grid.addWidget(QLabel("Add new"), 1, 0)
+        grid.addWidget(self.pairs, 1, 1)
+        grid.addWidget(self.addCurrencyButton, 1, 2)
+        self.newCurrencyGroup.setLayout(grid)
+        self.grid.addWidget(self.newCurrencyGroup, 1, 0)
+        self.setLayout(self.grid)
+
+    def __add_new_currency_connect(self):
+        self.__add_watcher(pair=self.pairs.currentText())
+
+    def __add_watcher(self, pair):
+        watcherGroup = QGroupBox("")
+        watcherGroup.setStyleSheet("QGroupBox{border: 0px;};")
+
+        lpair = QLabel(pair)
+        curPrice = QLabel(self.__get_pair_price(pair=pair))
+        watchPrice = QLineEdit("0.0000")
+
+        rgroup = QGroupBox("")
+        rgroup.setStyleSheet("QGroupBox{border: 0px;};")
+
+        rlayout = QHBoxLayout()
+        rless = QRadioButton("-")
+        requal = QRadioButton("=")
+        rgreat = QRadioButton("+")
+
+        requal.setChecked(True)
+        rlayout.addWidget(rless)
+        rlayout.addWidget(requal)
+        rlayout.addWidget(rgreat)
+        rgroup.setLayout(rlayout)
+
+        grid = QGridLayout()
+        grid.addWidget(lpair, 2, 0)
+        grid.addWidget(curPrice, 2, 1)
+        grid.addWidget(watchPrice, 2, 2)
+        grid.addWidget(rgroup, 2, 3)
+
+        watcherGroup.setLayout(grid)
+
+        self.watchers.append({
+            "group": watcherGroup, 
+            "pair": lpair, 
+            "uprice": watchPrice, 
+            "cprice": curPrice,
+            "radios": {"less": rless, "equal": requal, "great": rgreat}
+            })
+
+        self.grid.addWidget(watcherGroup, self.grid.rowCount(), 0)
+        self.setLayout(self.grid)
+
+    def __get_pair_price(self, pair):
+        for i in self.prices:
+            __pair = pair.replace("-", "")
+            if __pair == i.get("pair"):
+                return i.get("last")
+    
+    def update_watchers(self):
+        print "updating"
+        self.prices = binance.get_price()
+        for i in self.watchers:
+            _pair = i.get("pair").text()
+            i.get("cprice").setText(self.__get_pair_price(_pair.replace("-", "")))
+            cprice = float(i.get("cprice").text())
+            uprice = float(i.get("uprice").text())
+            if i.get("radios").get("less").isChecked():
+                if cprice < uprice:
+                    print "less"
+            elif i.get("radios").get("great").isChecked():
+                if cprice > uprice:
+                    print "great"
+            
 
 def main():
     app = QApplication([])
     foo = Launcher()
     foo.show()
     sys.exit(app.exec_())
+
+
+def _sort_pairs(pairs):
+    __pairs = []
+
+    for key, val in pairs.items():
+        for i in val:
+            __pairs.append(i + "-" + key)
+    return __pairs
 
 
 if __name__ == "__main__":
